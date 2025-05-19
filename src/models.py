e7d1e9c88eda6f19d7b1b760e4b8bafa62439acb
roma <romanover9000@gmail.com>
2025-05-19 21:28:47 +0300

    added rooms model and migration

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 
 from src.database import Base
 
@@ -10,3 +10,14 @@ class HotelsOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     location: Mapped[str]
+
+
+class RoomsOrm(Base):
+    __tablename__ = "rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
