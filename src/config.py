ebfcc2849e1ea9f4f818b579e7e9dbe856929c92
rpechurin <rpechurin@parovoz.tv>
2025-05-21 14:05:34 +0300

    db envs now separated from postgres and have abstract names

--- src/config.py
+++ src/config.py
@@ -4,15 +4,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
 
     DB_ECHO: bool = False
 
     @property
     def DB_URL(self):
-        return f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
 
