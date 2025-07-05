c4482c744f162ae70823454b8ad0863f328879e4
roma <romanover9000@gmail.com>
2025-07-05 19:17:07 +0300

    Added not found file optional error in LocalFileManager

--- src/api/routers/images.py
+++ src/api/routers/images.py
@@ -3,7 +3,7 @@ from fastapi.responses import FileResponse
 
 from src.exceptions import FileNotFoundException
 from src.service.file_storage import MediaFileStorageService
-# from src.tasks.tasks import resize_image
+from src.background_tasks.celery.tasks import resize_image
 
 router = APIRouter(prefix="/images", tags=["Images"])
 
@@ -23,9 +23,11 @@ def get_image(filepath: str):
 
 @router.post("")
 def upload_image(file: UploadFile):
+    filename = file.filename
+    file_object = file.file
     # write file and catch validation error
     try:
-        rel_filepath = MediaFileStorageService().create_file(file.filename, file.file)
+        rel_filepath = MediaFileStorageService().create_file(filename, file_object)
     except ValueError as err:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
@@ -33,5 +35,5 @@ def upload_image(file: UploadFile):
         )
 
     # background task - save resized image copies
-    # resize_image.delay(image_path)
+    resize_image.delay(filename, rel_filepath)
     return {"status": "Ok"}
--- src/background_tasks/celery/tasks.py
+++ src/background_tasks/celery/tasks.py
@@ -1,6 +1,11 @@
+from datetime import datetime, timezone
+import os
 from time import sleep
 
+from PIL import Image
+
 from src.background_tasks.celery.app import celery_app
+from src.service.file_storage import MediaFileStorageService
 
 
 @celery_app.task
@@ -10,4 +15,34 @@ def test_task():
     print("Test task finished")
 
 
-# todo resize image task
+@celery_app.task
+def resize_image(original_filename: str, rel_path: str):
+    file_service = MediaFileStorageService()
+
+    sizes = [300, 200, 100]
+
+    # Open image
+    image_abs_path = file_service.get_abs_filepath(rel_path)
+    img = Image.open(image_abs_path)
+
+    # Get image name and extension
+    name, ext = os.path.splitext(original_filename.strip().lower())
+
+    # Iterate over sizes
+    for size in sizes:
+        # Resize image
+        img_resized = img.resize((size, int(img.height * (size / img.width))), Image.Resampling.LANCZOS)
+
+        # Build image new name
+        new_filename = f"{name}_{size}px{ext}"
+        new_filename_obj = file_service.FilenameSchema(
+            original_filename=new_filename,
+            timestamp=datetime.now(timezone.utc).timestamp()
+        )
+
+        # New file absolute path
+        new_abs_path = file_service.get_abs_filepath(new_filename_obj.storage_filename, not_found_err=False)
+
+        # Save image
+        img_resized.save(new_abs_path)
+        print(f"Image saved: {new_abs_path}")
