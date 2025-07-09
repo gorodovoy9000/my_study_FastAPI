7c820c023f2048d9f80eafc91c71c9e473f55e8d
rpechurin <rpechurin@parovoz.tv>
2025-07-09 17:52:28 +0300

    Added first token tests.

---
+++ tests/integration_tests/test_auth.py
@@ -0,0 +1,12 @@
+from src.service.auth import AuthService
+
+
+def test_create_and_decode_access_token():
+    # test data
+    data = {'user_id': 1}
+    # create token
+    token = AuthService().create_access_token(data=data)
+    # decode token
+    payload = AuthService().decode_access_token(token)
+    # test result is correct
+    assert payload['user_id'] == 1
