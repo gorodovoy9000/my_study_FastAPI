5a01e82c880f1d3523232b856e4a37ed7b525e10
roma <romanover9000@gmail.com>
2025-07-14 17:10:13 +0300

    ImprovedAPI tests GET, POST for facilities.

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -2,18 +2,16 @@
 
 
 async def test_post_facilities(ac):
-    data = [
-        {'title': "wi-fi"},
-        {'title': "tv"},
-        {'title': "minibar"},
-    ]
-    for obj in data:
-        response = await ac.post("/facilities", json=obj)
-        print(response.json())
-        assert response.is_success
+    create_data = {'title': "wi-fi"}
+    response = await ac.post("/facilities", json=create_data)
+    assert response.is_success
+    response_data = response.json()
+    assert isinstance(response_data, dict)
+    assert "data" in response_data
+    assert response_data['data']['title'] == create_data['title']
 
 
 async def test_get_facilities(ac):
     response = await ac.get("/facilities")
-    print(response.json())
     assert response.is_success
+    assert isinstance(response.json(), list)
