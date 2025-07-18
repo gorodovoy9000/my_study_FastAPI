8c9df4c14a2823cc46e7c4eb0a67bb3eab230409
rpechurin <rpechurin@parovoz.tv>
2025-07-18 19:43:39 +0300

    Added parametrize to API test_add_booking.

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,12 +1,28 @@
-async def test_add_booking(db, authenticated_ac):
-    room_id = (await db.rooms.get_many_filtered(limit=1))[0].id
+import pytest
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2025-08-01", "2025-08-10", 200),
+    (1, "2025-08-02", "2025-08-11", 200),
+    (1, "2025-08-03", "2025-08-12", 200),
+    (1, "2025-08-04", "2025-08-13", 200),
+    (1, "2025-08-05", "2025-08-14", 200),
+    (1, "2025-08-06", "2025-08-15", 404),
+    (1, "2025-08-17", "2025-08-25", 200),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, authenticated_ac
+):
     data = {
         "room_id": room_id,
-        "date_from": "2025-08-01",
-        "date_to": "2025-08-10",
+        "date_from": date_from,
+        "date_to": date_to,
     }
     response = await authenticated_ac.post("/bookings", json=data)
-    assert response.is_success
-    response_payload = response.json()
-    assert isinstance(response_payload, dict)
-    assert "data" in response_payload
+    if response.is_success:
+        response_payload = response.json()
+        assert isinstance(response_payload, dict)
+        assert "data" in response_payload
+    else:
+        assert response.status_code == status_code
