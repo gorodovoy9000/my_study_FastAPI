bff22a10c78113563b3ee50b217ebff8c77d68a6
roma <romanover9000@gmail.com>
2025-07-19 15:06:48 +0300

    Added test_add_and_get_bookings with fixture.

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,4 +1,14 @@
+from sqlalchemy import delete
 import pytest
+
+from src.models.bookings import BookingsOrm
+
+
+@pytest.fixture(scope="class")
+async def delete_all_bookings(db):
+    stmt = delete(BookingsOrm)
+    await db.session.execute(stmt)
+    await db.session.commit()
 
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
@@ -26,3 +36,32 @@ async def test_add_booking(
         assert "data" in response_payload
     else:
         assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, my_bookings_count", [
+    (1, "2025-08-01", "2025-08-10", 1),
+    (1, "2025-08-02", "2025-08-11", 2),
+    (1, "2025-08-03", "2025-08-12", 3),
+])
+async def test_add_and_get_bookings(
+        room_id, date_from, date_to, my_bookings_count,
+        db, authenticated_ac, delete_all_bookings
+):
+    # create booking
+    data = {
+        "room_id": room_id,
+        "date_from": date_from,
+        "date_to": date_to,
+    }
+    # test response data
+    response = await authenticated_ac.post("/bookings", json=data)
+    assert response.is_success
+    response_payload = response.json()
+    assert isinstance(response_payload, dict)
+    assert "data" in response_payload
+    # check my bookings
+    response = await authenticated_ac.get("/bookings/me")
+    assert response.is_success
+    response_payload = response.json()
+    assert isinstance(response_payload, list)
+    assert len(response_payload) == my_bookings_count
