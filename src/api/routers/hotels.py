518251a41eff88f568ddd17999a6222f03ca7941
rpechurin <rpechurin@parovoz.tv>
2025-07-25 14:52:59 +0300

    Implemented hotels service.

--- src/api/routers/hotels.py
+++ src/api/routers/hotels.py
@@ -8,6 +8,7 @@ from src.exceptions import ForeignKeyException, NotFoundException
 from src.api.dependencies import DBDep, PaginationDep
 from src.api.exceptions import validate_date_to_is_bigger_than_date_from
 from src.schemas.hotels import HotelsSchema, HotelsPatchSchema, HotelsWriteSchema
+from src.services.hotels import HotelService
 
 router = APIRouter(prefix="/hotels", tags=["hotels"])
 
@@ -27,7 +28,7 @@ async def get_hotels(
     ] = None,
 ) -> list[HotelsSchema]:
     validate_date_to_is_bigger_than_date_from(date_from=date_from, date_to=date_to)
-    data = await db.hotels.get_hotels_with_vacant_rooms(
+    data = await HotelService(db).get_hotels_filtered(
         date_from=date_from,
         date_to=date_to,
         location=location,
@@ -41,7 +42,7 @@ async def get_hotels(
 @router.get("/{hotel_id}")
 async def get_hotel(db: DBDep, hotel_id: int) -> HotelsSchema:
     try:
-        data = await db.hotels.get_one(id=hotel_id)
+        data = await HotelService(db).get_hotel(hotel_id=hotel_id)
     except NotFoundException:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hotel not found")
     return data
@@ -49,42 +50,37 @@ async def get_hotel(db: DBDep, hotel_id: int) -> HotelsSchema:
 
 @router.post("", status_code=201)
 async def create_hotel(db: DBDep, schema_create: HotelsWriteSchema):
-    data = await db.hotels.add(schema_create)
-    # transaction commit MUST stay here!
-    await db.commit()
+    data = await HotelService(db).add_hotel(schema_create)
     return {"status": "Ok", "data": data}
 
 
 @router.delete("/{hotel_id}", status_code=204)
 async def delete_hotel(db: DBDep, hotel_id: int):
     try:
-        await db.hotels.delete(id=hotel_id)
+        await HotelService(db).delete_hotel(hotel_id=hotel_id)
     except NotFoundException:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hotel not found")
     except ForeignKeyException:
         msg = "Cannot delete hotel that has rooms"
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=msg)
-    await db.commit()
     return {"status": "Ok"}
 
 
 @router.put("/{hotel_id}", status_code=204)
 async def update_hotel(db: DBDep, hotel_id: int, schema_update: HotelsWriteSchema):
     try:
-        await db.hotels.edit(schema_update, id=hotel_id)
+        await HotelService(db).edit_hotel(hotel_id=hotel_id, data=schema_update)
     except NotFoundException:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hotel not found")
-    await db.commit()
     return {"status": "Ok"}
 
 
 @router.patch("/{hotel_id}", status_code=204)
-async def partial_update_hotel(
+async def patch_hotel(
     db: DBDep, hotel_id: int, schema_patch: HotelsPatchSchema
 ):
     try:
-        await db.hotels.edit(schema_patch, partial_update=True, id=hotel_id)
+        await HotelService(db).edit_hotel_partially(hotel_id=hotel_id, data=schema_patch)
     except NotFoundException:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hotel not found")
-    await db.commit()
     return {"status": "Ok"}
---
+++ src/services/hotels.py
@@ -0,0 +1,48 @@
+from datetime import date
+
+# from src.api.exceptions import validate_date_to_is_bigger_than_date_from
+from src.schemas.hotels import HotelsWriteSchema, HotelsPatchSchema, HotelsSchema
+from src.services.base import BaseService
+
+
+class HotelService(BaseService):
+    async def get_hotels_filtered(
+            self,
+            date_from: date,
+            date_to: date,
+            location: str | None,
+            title: str | None,
+            limit: int,
+            offset: int,
+    ) -> list[HotelsSchema]:
+        # validate_date_to_is_bigger_than_date_from(date_from=date_from, date_to=date_to)
+        hotels =  await self.db.hotels.get_hotels_with_vacant_rooms(
+            date_from=date_from,
+            date_to=date_to,
+            location=location,
+            title=title,
+            limit=limit,
+            offset=offset,
+        )
+        return hotels
+
+    async def get_hotel(self, hotel_id: int) -> HotelsSchema:
+        hotel = await self.db.hotels.get_one(id=hotel_id)
+        return hotel
+
+    async def add_hotel(self, data: HotelsWriteSchema) -> HotelsSchema:
+        hotel = await self.db.hotels.add(data)
+        await self.db.commit()
+        return hotel
+
+    async def edit_hotel(self, hotel_id: int, data: HotelsWriteSchema) -> None:
+        await self.db.hotels.edit(data, id=hotel_id)
+        await self.db.commit()
+
+    async def edit_hotel_partially(self, hotel_id: int, data: HotelsPatchSchema) -> None:
+        await self.db.hotels.edit(data, partial_update=True, id=hotel_id)
+        await self.db.commit()
+
+    async def delete_hotel(self, hotel_id: int) -> None:
+        await self.db.hotels.delete(id=hotel_id)
+        await self.db.commit()
