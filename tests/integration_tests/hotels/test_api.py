e027d327752630e6d6526ffdb14118ba0ce1b01d
rpechurin <rpechurin@parovoz.tv>
2025-08-12 18:31:07 +0300

    Added hotel API CRUD test.

--- tests/integration_tests/hotels/test_api.py
+++ tests/integration_tests/hotels/test_api.py
@@ -8,3 +8,41 @@ async def test_get_hotels(ac):
     )
     print(response.json())
     assert response.is_success
+
+
+async def test_hotel_crud(ac):
+    # create hotel
+    post_data = {
+        "title": "Ukhovertka",
+        "location": "Somewhere in jungle"
+    }
+    response = await ac.post("/hotels", json=post_data)
+    assert response.is_success
+    response_data = response.json()
+    assert isinstance(response_data, dict)
+    created_data = response_data["data"]
+    assert created_data["title"] == post_data["title"]
+
+    # get hotel
+    response = await ac.get(f"/hotels/{created_data['id']}")
+    assert response.is_success
+
+    # put hotel
+    put_data = {
+        "title": "Nautilus",
+        "location": "Submarine somewhere in ocean"
+    }
+    response = await ac.put(f"/hotels/{created_data['id']}", json=put_data)
+    assert response.is_success
+
+    # patch hotel
+    patch_data = {
+        "location": "Somewhere in pacific"
+    }
+    response = await ac.patch(f"/hotels/{created_data['id']}", json=patch_data)
+    assert response.is_success
+
+    # delete hotel
+    response = await ac.delete(f"/hotels/{created_data['id']}")
+    assert response.is_success
+
